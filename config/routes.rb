0af691236d3e600c0b5429dc22ced31945e6eaa1
gnakano <genjinakano@gmail.com>
2015-02-14 08:27:51 +0000

    Created welcome controller with index and about views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
+# get 'welcome/index'
+# get 'welcome/about'
+
 Rails.application.routes.draw do
+
+  get 'welcome/index'
+
+  get 'welcome/about'
+  
+  root to: 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
